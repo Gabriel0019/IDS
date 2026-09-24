7dea248e159c0342be7841c3938f20519df0ee55
Gabriel <j.gab.a2004@gmail.com>
2026-09-24 15:28:47 -0400

    Implement packet analysis functionality in IDS with alerts for port scanning

--- ids.py
+++ ids.py
@@ -1,9 +1,54 @@
-from scapy.all import sniff 
+from scapy.all import sniff, IP, TCP, UDP
+from collections import defaultdict
+import time
 
-def ver_paquete(paquete): 
-    print(paquete.summary()) 
+puertos_ip = defaultdict(set)
+tiempo_inicio = defaultdict(float)
+
+limite_puertos = 10
+ventanas_tiempo = 10
+
+def analizar_paquete(paquete):
+    if IP not in paquete:
+        return
+    ip_origen = paquete[IP].src
+
+    if TCP in paquete:
+        puerto_destino = paquete[TCP].dport
+
+    elif UDP in paquete:
+        puerto_destino = paquete[UDP].dport
+
+    else:
+        return
+
+    ahora = time.time()
+
+    if not tiempo_inicio[ip_origen]:
+        tiempo_inicio[ip_origen] = ahora
+
+    tiempo_transcurrido = ahora - tiempo_inicio[ip_origen]
+
+    if tiempo_transcurrido > ventanas_tiempo:
+        puertos_ip[ip_origen].clear()
+        tiempo_inicio[ip_origen] = ahora
+
+    puertos_ip[ip_origen].add(puerto_destino)
+
+    cantidad = len(puertos_ip[ip_origen])
+    print(f"{ip_origen} -> puerto {puerto_destino}" f"| Puertos distintos: {cantidad}")
+
+    if cantidad >= limite_puertos:
+        print(f"Alerta: Posible escaneo de puertos desde {ip_origen} (puertos distintos: {cantidad})")
+        puertos_ip[ip_origen].clear()
+        tiempo_inicio[ip_origen] = ahora
+
+    puertos_ip[ip_origen].clear()
+
+    tiempo_inicio[ip_origen] = ahora
+
 
 print("IDS iniciado...")
-print("Esperando paquetes...")
+print("Analizando paquetes...")
 
-sniff(prn=ver_paquete, filter="ip", store=0)
+sniff(prn=analizar_paquete, filter="ip", store=0)
